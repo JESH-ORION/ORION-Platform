b219879a184e739447b3a438ad5289de3aac1a51
jose ednardo <jose.ednardo@bluemond.com.br>
2026-08-02 13:57:52 -0300

    feat(database): cria estrutura inicial do banco ORION

--- scripts/database/scrip_criacao_banco_ORION_1.sql
+++ scripts/database/scrip_criacao_banco_ORION_1.sql
@@ -1,15 +1,17 @@
 -- =============================================================================
 -- SCRIPT DE CRIAÇÃO E CONFIGURAÇÃO DO BANCO DE DADOS POSTGRESQL
+-- PROJETO ORION
 -- =============================================================================
 
--- 1. Criação do Banco de Dados
--- Define a codificação UTF8 (para acentuação e caracteres especiais)
--- e as regras de ordenação de texto.
 CREATE DATABASE orion_dev
-    WITH 
+    WITH
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'pt_BR.UTF-8'
-    LC_CTYPE = 'pt_BR.UTF-8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
+	
+COMMENT ON DATABASE orion_dev
+    IS 'Banco de dados principal da plataforma ORION';
+
+ALTER DATABASE orion_dev
+    SET timezone TO 'America/Sao_Paulo';	
